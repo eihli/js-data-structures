3bc6b868ebb00121ee4125816fec8f4be1c77f22
Eric Ihli <ericihli@gmail.com>
2016-03-30 18:49:30 -0700

    Initialize Set

---
+++ src/set.js
@@ -0,0 +1,5 @@
+function Set() {
+
+}
+
+module.exports = Set;
---
+++ tests/set.js
@@ -0,0 +1,12 @@
+var Set = require('../src/set.js');
+
+describe('set', function() {
+  var mySet;
+  beforeEach(function() {
+    mySet = new Set();
+  });
+
+  it('should exist', function() {
+    mySet.should.be.an.instanceof(Set);
+  });
+});
