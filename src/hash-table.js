e794c4c1ec817ac439192e1ed63b1c6278dc6a68
Eric Ihli <ericihli@gmail.com>
2016-03-31 20:38:48 -0700

    Refactor to use private method

--- src/hash-table.js
+++ src/hash-table.js
@@ -1,22 +1,28 @@
-function HashTable() {
-  this._storage = [];
-}
-
-HashTable.prototype.add = function(key, value) {
-  this._storage.push([key, value]);
-};
-
-HashTable.prototype.get = function(key) {
-  for (var i = 0; i < this.size(); i++) {
-    if (this._storage[i][0] === key) {
-      return this._storage[i][1];
-    }
+module.exports = (function() {
+  function HashTable() {
+    this._storage = [];
   }
-  return undefined;
-};
 
-HashTable.prototype.size = function() {
-  return this._storage.length;
-};
+  HashTable.prototype.add = function(key, value) {
+    this._storage.push([key, value]);
+  };
 
-module.exports = HashTable;
+  HashTable.prototype.get = function(key) {
+    return getValueAt.call(this, key);
+  };
+
+  HashTable.prototype.size = function() {
+    return this._storage.length;
+  };
+
+  function getValueAt(key) {
+    for (var i = 0; i < this.size(); i++) {
+      if (this._storage[i][0] === key) {
+        return this._storage[i][1];
+      }
+    }
+    return undefined;
+  }
+
+  return HashTable;
+})();
